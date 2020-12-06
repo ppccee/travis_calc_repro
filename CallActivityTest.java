6990f6551132cf1c29ad5774ddcf38ae01474c8b
kimjangsung <62543720+kimjangsung@users.noreply.github.com>
2020-12-07 02:48:33 +0900

    Create CallActivityTest.java

---
+++ CallActivityTest.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CallActivityTest {
+
+    @Test
+    public void onCreate() {
+        assertNotNull(0);
+    }
+}
